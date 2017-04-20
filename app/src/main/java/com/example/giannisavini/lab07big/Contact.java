261510606faa0743227dc7f8a14b1e8365770fa4
Gianni Savini <gianni.savini@studio.unibo.it>
2017-04-20 15:45:59 +0200

    I've added class Contact for managing contacts

---
+++ app/src/main/java/com/example/giannisavini/lab07big/Contact.java
@@ -0,0 +1,68 @@
+package com.example.giannisavini.lab07big;
+
+import android.database.Cursor;
+import android.provider.BaseColumns;
+
+/**
+ * Created by gianni.savini on 20/04/2017.
+ */
+public class Contact implements BaseColumns {
+    //variabili statiche  per la gestione della tabella
+    public static final String TABLE_NAME = "people";
+
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_SURNAME = "surname";
+    public static final String COLUMN_NUMBER = "number";
+    public static final String COLUMN_EMAIL = "e_mail";
+
+    private int id;
+    private String name;
+    private String surname;
+    private String number;
+    private String email;
+
+    public Contact(final String name,final String surname,final String number,final String email) {
+        this.name = name;
+        this.surname = surname;
+        this.number = number;
+        this.email = email;
+    }
+
+    public Contact(final Cursor cursor) {
+        //Chiamo il costruttore sopra, con i parametri del cursor
+        this(cursor.getString(cursor.getColumnIndex(COLUMN_NAME)), cursor.getString(cursor.getColumnIndex(COLUMN_SURNAME)),
+                cursor.getString(cursor.getColumnIndex(COLUMN_EMAIL)), cursor.getString(cursor.getColumnIndex(COLUMN_NUMBER)));
+        this.id = cursor.getInt(cursor.getColumnIndex(_ID));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "email='" + email + '\'' +
+                ", number='" + number + '\'' +
+                ", surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
